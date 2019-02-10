7a2297204a01526193d9c1d38048045ba0f19fc2
Malik Iscandari <miscandari@mail.sfsu.edu>
2019-02-10 15:27:49 -0800

    Initial Commit

---
+++ PianoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PianoApp
+//
+//  Created by Malik on 2/10/19.
+//  Copyright © 2019 Kilam Creatives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
